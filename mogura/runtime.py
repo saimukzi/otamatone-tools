fb6636483db9d9fb700a72df215125637951e83b
saimukzi <saimukzi@hiauntie.com>
2023-01-07 15:02:30 +0800

    move timer pool feature to timer_pool

--- mogura/runtime.py
+++ mogura/runtime.py
@@ -1,6 +1,7 @@
 import freq_timer
 import pygame
 import time
+import timer_pool
 
 FPS = 60
 EPS = FPS * 10
@@ -9,43 +10,18 @@ class Runtime:
 
     def __init__(self):
         self.running = None
-        self.timer_list = [] 
+        self.timer_pool = timer_pool.TimerPool()
 
     def run(self):
         pygame.init()
     
         screen = pygame.display.set_mode((1280,720))
         
-        self.t0 = time.time()
+        t0 = time.time()
+        self.timer_pool.add_timer(freq_timer.FreqTimer(self, t0, FPS, lambda sec: self.screen_tick()))
+        self.timer_pool.add_timer(freq_timer.FreqTimer(self, t0+1/2/EPS, EPS, lambda sec: self.event_tick()))
         
-        self.screen_timer = freq_timer.FreqTimer(self, self.t0, FPS, lambda sec: self.screen_tick())
-        self.timer_list.append(self.screen_timer)
-
-        self.event_timer = freq_timer.FreqTimer(self, self.t0+1/2/EPS, EPS, lambda sec: self.event_tick())
-        self.timer_list.append(self.event_timer)
-        
-        self.running = True
-        while self.running:
-            now_sec = time.time()
-            #print(f'loop now_sec={now_sec}')
-
-            active_timer_sec_idx_timer_list = []
-            for i,timer in enumerate(self.timer_list):
-                next_sec = timer.next_sec()
-                if next_sec > now_sec: continue
-                active_timer_sec_idx_timer_list.append((next_sec,i,timer))
-            active_timer_sec_idx_timer_list.sort()
-
-            for _,_,timer in active_timer_sec_idx_timer_list:
-                timer.run(now_sec)
-                timer.update_next_sec(now_sec)
-
-            next_sec = self.timer_list
-            next_sec = map(lambda i:i.next_sec(),next_sec)
-            next_sec = min(next_sec)
-
-            now_sec = time.time()
-            time.sleep(max(next_sec-now_sec,0))
+        self.timer_pool.run()
 
     def screen_tick(self):
         #print('screen_tick')
@@ -56,7 +32,7 @@ class Runtime:
         #print('event_tick')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
+                self.timer_pool.stop()
 
 
 instance = None
---
+++ mogura/timer_pool.py
@@ -0,0 +1,37 @@
+import time
+
+class TimerPool:
+
+    def __init__(self):
+        self.running = None
+        self.timer_list = []
+
+    def add_timer(self, timer):
+        self.timer_list.append(timer)
+
+    def run(self):
+        self.running = True
+
+        while self.running:
+            now_sec = time.time()
+
+            active_timer_sec_idx_timer_list = []
+            for i,timer in enumerate(self.timer_list):
+                next_sec = timer.next_sec()
+                if next_sec > now_sec: continue
+                active_timer_sec_idx_timer_list.append((next_sec,i,timer))
+            active_timer_sec_idx_timer_list.sort()
+
+            for _,_,timer in active_timer_sec_idx_timer_list:
+                timer.run(now_sec)
+                timer.update_next_sec(now_sec)
+
+            next_sec = self.timer_list
+            next_sec = map(lambda i:i.next_sec(),next_sec)
+            next_sec = min(next_sec)
+
+            now_sec = time.time()
+            time.sleep(max(next_sec-now_sec,0))
+
+    def stop(self):
+        self.running = False
