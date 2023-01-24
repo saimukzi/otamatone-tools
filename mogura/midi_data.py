6699e5d38aa7b2907cde1e602fee3ca71a4a73c5
saimukzi <saimukzi@hiauntie.com>
2023-01-24 17:03:15 +0800

    refactor track chop

--- mogura/midi_data.py
+++ mogura/midi_data.py
@@ -1,3 +1,6 @@
+import copy
+import itertools
+#import json
 import mido
 
 
@@ -14,6 +17,7 @@ def mid_to_data(mid):
 
 def track_to_data(track,ticks_per_beat):
     track_data = {}
+    track_data['ticks_per_beat'] = ticks_per_beat
     track_data['name'] = track.name
     track_data['noteev_list'] = track_to_noteev_list(track)
     end_tick = track_data['noteev_list'][-1]['tick']
@@ -151,9 +155,10 @@ def track_to_tempo_list(track,end_tick,ticks_per_beat):
 
 
 def tick_to_sec6tpb(tick, tempo_list):
-    if tick <= 0:
+    #print(f'HGWHMWQKXA tick={tick}, tempo_list={tempo_list}')
+    if tick < tempo_list[0]['tick1']:
         tempo = tempo_list[0]
-    if tick >= tempo_list[-1]['tick0']:
+    elif tick >= tempo_list[-1]['tick0']:
         tempo = tempo_list[-1]
     else:
         tempo = tempo_list
@@ -165,14 +170,218 @@ def tick_to_sec6tpb(tick, tempo_list):
 
 
 def sec6tpb_to_tick(sec6tpb, tempo_list):
+    #print(f'IVBIQPSQAA sec6tpb={sec6tpb}, tempo_list={tempo_list}')
     if sec6tpb <= 0:
         tempo = tempo_list[0]
     if sec6tpb >= tempo_list[-1]['sec6tpb0']:
         tempo = tempo_list[-1]
     else:
         tempo = tempo_list
-        tempo = filter(lambda i:i['sec6tpb0']<=tick,sec6tpb)
-        tempo = filter(lambda i:i['sec6tpb1']>tick,sec6tpb)
-        assert(len(tempo))
+        tempo = filter(lambda i:i['sec6tpb0']<=sec6tpb,tempo)
+        tempo = filter(lambda i:i['sec6tpb1']>sec6tpb,tempo)
+        tempo = list(tempo)
+        #print(f'DWKUYTSJIC sec6tpb={sec6tpb}, tempo={tempo}')
+        assert(len(tempo)==1)
         tempo = tempo[0]
     return (sec6tpb-tempo['sec6tpb0'])/tempo['tempo']+tempo['tick0']
+
+
+def track_data_chop_tick(track_data, start_bar_tick, start_note_tick, end_note_tick, end_bar_tick):
+    start_bar_sec6tpb  = tick_to_sec6tpb(start_bar_tick,  track_data['tempo_list'])
+    start_note_sec6tpb = tick_to_sec6tpb(start_note_tick, track_data['tempo_list'])
+    end_note_sec6tpb   = tick_to_sec6tpb(end_note_tick,   track_data['tempo_list'])
+    end_bar_sec6tpb    = tick_to_sec6tpb(end_bar_tick,    track_data['tempo_list'])
+
+    out_track_data = copy.deepcopy(track_data)
+    noteev_list = out_track_data['noteev_list']
+    noteev_list = filter(_track_data_chop_time_noteev_filter(start_note_tick, end_note_tick), noteev_list)
+    noteev_list = list(noteev_list)
+    for noteev in noteev_list:
+        noteev['tick0'] = max(noteev['tick0'], start_note_tick)
+        noteev['tick1'] = min(noteev['tick1'], end_note_tick)
+        noteev['tick'] = noteev['tick0']
+        noteev['sec6tpb0'] = max(noteev['sec6tpb0'], start_note_sec6tpb)
+        noteev['sec6tpb1'] = min(noteev['sec6tpb1'], end_note_sec6tpb)
+        noteev['sec6tpb'] = noteev['sec6tpb0']
+
+    noteev_off_list = map(_on_noteev_to_off_noteev, noteev_list)
+    noteev_off_list = list(noteev_off_list)
+
+    noteev_list = noteev_list + noteev_off_list
+    noteev_list = sorted(noteev_list, key=_noteev_sort_key)
+
+    out_track_data['noteev_list'] = noteev_list
+
+    bar_list = out_track_data['bar_list']
+    bar_list = filter(lambda i:i>=start_bar_tick ,bar_list)
+    bar_list = filter(lambda i:i<=end_bar_tick ,bar_list)
+    bar_list = list(bar_list)
+    out_track_data['bar_list'] = bar_list
+
+    out_track_data['bar_set'] = set(bar_list)
+
+    tempo_list = out_track_data['tempo_list']
+    tempo_list = filter(lambda i:i['tick1']> start_bar_tick,tempo_list)
+    tempo_list = filter(lambda i:i['tick0']< end_bar_tick  ,tempo_list)
+    tempo_list = list(tempo_list)
+    for tempo in tempo_list:
+        tempo['tick0'] = max(tempo['tick0'], start_bar_tick)
+        tempo['tick1'] = min(tempo['tick1'], end_bar_tick)
+        tempo['sec6tpb0'] = max(tempo['sec6tpb0'], start_bar_sec6tpb)
+        tempo['sec6tpb1'] = min(tempo['sec6tpb1'], end_bar_sec6tpb)
+    out_track_data['tempo_list'] = tempo_list
+    return out_track_data
+
+def _track_data_chop_time_noteev_filter(start_tick, end_tick):
+    def _ret(noteev):
+        if noteev['type'] != 'on': return False
+        if noteev['tick0'] >= end_tick:   return False
+        if noteev['tick1'] <  start_tick: return False
+        return True
+    return _ret
+
+def _on_noteev_to_off_noteev(noteev):
+    ret_data = {
+        'tick':noteev['tick1'],
+        'sec6tpb':noteev['sec6tpb1'],
+        'type':'off',
+        'channel':noteev['channel'],
+        'pitch':noteev['pitch'],
+    }
+    return ret_data
+
+def _noteev_sort_key(noteev):
+    TYPE_TO_IDX={'off':0,'on':1}
+    return (
+        noteev['tick'],
+        TYPE_TO_IDX[noteev['type']],
+        noteev['channel'],
+        noteev['pitch'],
+    )
+
+def _tempo_sort_key(tempo):
+    return (
+        tempo['tick0'],
+        tempo['tick1'],
+        tempo['sec6tpb0'],
+        tempo['sec6tpb1'],
+        tempo['tempo'],
+    )
+
+def track_data_move_tick(track_data, tick_diff):
+    out_track_data = copy.deepcopy(track_data)
+    noteev_list = out_track_data['noteev_list']
+    for noteev in noteev_list:
+        if 'tick' in noteev:  noteev['tick']+=tick_diff
+        if 'tick0' in noteev: noteev['tick0']+=tick_diff
+        if 'tick1' in noteev: noteev['tick1']+=tick_diff
+
+    bar_list = out_track_data['bar_list']
+    bar_list = map(lambda i:i+tick_diff, bar_list)
+    bar_list = list(bar_list)
+    out_track_data['bar_list'] = bar_list
+
+    tempo_list = out_track_data['tempo_list']
+    for tempo in tempo_list:
+        tempo['tick0'] += tick_diff
+        tempo['tick1'] += tick_diff
+    return out_track_data
+
+def track_data_move_sec6tpb(track_data, sec6tpb_diff):
+    out_track_data = copy.deepcopy(track_data)
+    noteev_list = out_track_data['noteev_list']
+    for noteev in noteev_list:
+        if 'sec6tpb' in noteev:  noteev['sec6tpb']+=sec6tpb_diff
+        if 'sec6tpb0' in noteev: noteev['sec6tpb0']+=sec6tpb_diff
+        if 'sec6tpb1' in noteev: noteev['sec6tpb1']+=sec6tpb_diff
+
+    tempo_list = out_track_data['tempo_list']
+    for tempo in tempo_list:
+        tempo['sec6tpb0'] += sec6tpb_diff
+        tempo['sec6tpb1'] += sec6tpb_diff
+    return out_track_data
+
+# def merge_track_data(track_data_list):
+#     output_data = {}
+#     
+#     ticks_per_beat = map(lambda i:i['ticks_per_beat'],m_data_list)
+#     ticks_per_beat = set(ticks_per_beat)
+#     assert(len(ticks_per_beat)==1)
+#     ticks_per_beat = list(ticks_per_beat)[0]
+#     output_data['ticks_per_beat'] = ticks_per_beat
+# 
+#     track_cnt = map(lambda i:len(i['track_list']),m_data_list)
+#     track_cnt = max(track_cnt)
+#     output_data['track_list'] = []
+#     for track_id in range(track_cnt):
+#         src_track_data_list = filter(lambda i:len(i['track_list'])>track_id, m_data_list)
+#         src_track_data_list = list(map(lambda i:i['track_list'][track_id], m_data_list))
+#         
+#         output_track_data = _merge_track_data(src_track_data_list)
+#         output_data['track_list'].append(output_track_data)
+# 
+#     return output_data
+
+def merge_track_data(src_track_data_list):
+    output_track_data = {}
+    output_track_data['name'] = src_track_data_list[0]['name']
+
+    noteev_list = itertools.chain(*list(map(lambda i:i['noteev_list'],src_track_data_list)))
+    noteev_list = sorted(noteev_list, key=_noteev_sort_key)
+    on_channel_pitch_set = set()
+    #print(noteev_list)
+    #print(json.dumps(noteev_list,indent=2))
+    for noteev in noteev_list:
+        #print(f'BYFKBGRYBS noteev={noteev}')
+        channel_pitch = (noteev['channel'], noteev['pitch'])
+        if noteev['type'] == 'on':
+            if channel_pitch in on_channel_pitch_set:
+                assert(False)
+            on_channel_pitch_set.add(channel_pitch)
+        elif noteev['type'] == 'off':
+            assert(channel_pitch in on_channel_pitch_set)
+            on_channel_pitch_set.remove(channel_pitch)
+        else:
+            assert(False)
+    output_track_data['noteev_list'] = noteev_list
+
+    bar_list = itertools.chain(*list(map(lambda i:i['bar_list'],src_track_data_list)))
+    bar_list = sorted(list(set(bar_list)))
+    output_track_data['bar_list'] = bar_list
+
+    output_track_data['bar_set'] = set(output_track_data['bar_list'])
+
+    src_tempo_list = itertools.chain(*list(map(lambda i:i['tempo_list'],src_track_data_list)))
+    src_tempo_list = sorted(src_tempo_list, key=_tempo_sort_key)
+    #print(json.dumps(tempo_list,indent=2))
+    #for i in range(len(tempo_list)-1):
+    #    tempo0 = tempo_list[i]
+    #    tempo1 = tempo_list[i+1]
+    #    assert(tempo0['tick1']==tempo1['tick0'])
+    #    assert(tempo0['sec6tpb1']==tempo1['sec6tpb0'])
+    #output_track_data['tempo_list'] = tempo_list
+    #print(f'AVYZPMFEOG src_tempo_list={src_tempo_list}')
+    out_tempo_list = []
+    out_tempo_list.append(src_tempo_list[0])
+    for src_tempo in src_tempo_list[1:]:
+        assert(src_tempo['tick0']>=out_tempo_list[-1]['tick1'])
+        assert(src_tempo['sec6tpb0']>=out_tempo_list[-1]['sec6tpb1'])
+        if src_tempo['tick0'] > out_tempo_list[-1]['tick1']:
+            tick = src_tempo['tick0'] - out_tempo_list[-1]['tick1']
+            sec6tpb = src_tempo['sec6tpb0'] - out_tempo_list[-1]['sec6tpb1']
+            tempo = {
+                'tempo': sec6tpb/tick,
+                'tick0': out_tempo_list[-1]['tick1'],
+                'sec6tpb0': out_tempo_list[-1]['sec6tpb1'],
+                'tick1': src_tempo['tick0'],
+                'sec6tpb1': src_tempo['sec6tpb0'],
+            }
+            out_tempo_list.append(tempo)
+        out_tempo_list.append(src_tempo)
+    #print(f'TVNNIFCPIK out_tempo_list={out_tempo_list}')
+    output_track_data['tempo_list'] = out_tempo_list
+
+    output_track_data['max_pitch'] = max(map(lambda i:i['max_pitch'],src_track_data_list))
+    output_track_data['min_pitch'] = min(map(lambda i:i['min_pitch'],src_track_data_list))
+    
+    return output_track_data
--- mogura/play_state.py
+++ mogura/play_state.py
@@ -13,7 +13,8 @@ class PlayState(note_state.NoteState):
         self.lctrl_down = False
         
         self.start_sec = None
-        self.track_list = None
+        #self.track_list = None
+        self.loop_sec6tpb = None
 
     def screen_tick(self, screen, sec):
         super().screen_tick(screen, sec)
@@ -22,7 +23,8 @@ class PlayState(note_state.NoteState):
         vision_offset_y -= self.start_sec
         vision_offset_y *= 1000000
         vision_offset_y *= self.matric_ticks_per_beat
-        vision_offset_y %= self.track_data['sec6tpb']
+        # vision_offset_y %= self.track_data['sec6tpb']
+        vision_offset_y %= self.loop_sec6tpb
         vision_offset_y = midi_data.sec6tpb_to_tick(vision_offset_y, self.track_data['tempo_list'])
         vision_offset_y /= self.matric_ticks_per_beat
         vision_offset_y *= self.matric_cell_width
@@ -36,105 +38,130 @@ class PlayState(note_state.NoteState):
     def on_active(self):
         self.start_sec = time.time()
 
-        src_track_data = self.runtime.midi_data['track_list'][0]
-        
-        play_tick_list    = list(map(lambda i:i*self.runtime.midi_data['ticks_per_beat'],self.runtime.play_beat_list))
-        play_sec6tpb_list = list(map(lambda i:midi_data.tick_to_sec6tpb(i,src_track_data['tempo_list']),play_tick_list))
+#        src_track_data = self.runtime.midi_data['track_list'][0]
+#        
+#        play_tick_list    = list(map(lambda i:i*self.runtime.midi_data['ticks_per_beat'],self.runtime.play_beat_list))
+#        play_sec6tpb_list = list(map(lambda i:midi_data.tick_to_sec6tpb(i,src_track_data['tempo_list']),play_tick_list))
+#
+#        self.track_data = {}
+#
+#        noteev_list = src_track_data['noteev_list']
+#        noteev_list = filter(lambda i:i['type']=='on', noteev_list)
+#        noteev_list = filter(lambda i:i['tick1']>=play_tick_list[1],noteev_list)
+#        noteev_list = filter(lambda i:i['tick0']<play_tick_list[2],noteev_list)
+#        noteev_list = list(noteev_list)
+#        noteev_list = copy.deepcopy(noteev_list)
+#        for noteev in noteev_list:
+#            noteev['tick']  -= play_tick_list[0]
+#            if 'tick0' in noteev: noteev['tick0'] -= play_tick_list[0]
+#            if 'tick1' in noteev: noteev['tick1'] -= play_tick_list[0]
+#            noteev['sec6tpb']  -= play_sec6tpb_list[0]
+#            if 'sec6tpb0' in noteev: noteev['sec6tpb0'] -= play_sec6tpb_list[0]
+#            if 'sec6tpb1' in noteev: noteev['sec6tpb1'] -= play_sec6tpb_list[0]
+#
+#        play_noteev_list = copy.deepcopy(noteev_list)
+#
+#        noteev_list0 = copy.deepcopy(noteev_list)
+#        for noteev in noteev_list0:
+#            noteev['tick']  -= play_tick_list[3]-play_tick_list[0]
+#            if 'tick0' in noteev: noteev['tick0'] -= play_tick_list[3]-play_tick_list[0]
+#            if 'tick1' in noteev: noteev['tick1'] -= play_tick_list[3]-play_tick_list[0]
+#            noteev['sec6tpb']  -= play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#            if 'sec6tpb0' in noteev: noteev['sec6tpb0'] -= play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#            if 'sec6tpb1' in noteev: noteev['sec6tpb1'] -= play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#
+#        noteev_list1 = copy.deepcopy(noteev_list)
+#        for noteev in noteev_list1:
+#            noteev['tick']  += play_tick_list[3]-play_tick_list[0]
+#            if 'tick0' in noteev: noteev['tick0'] += play_tick_list[3]-play_tick_list[0]
+#            if 'tick1' in noteev: noteev['tick1'] += play_tick_list[3]-play_tick_list[0]
+#            noteev['sec6tpb']  += play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#            if 'sec6tpb0' in noteev: noteev['sec6tpb0'] += play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#            if 'sec6tpb1' in noteev: noteev['sec6tpb1'] += play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#
+#        noteev_list = noteev_list0+noteev_list+noteev_list1
+#
+#        self.track_data['noteev_list'] = noteev_list
+#        
+#        bar_list = src_track_data['bar_list']
+#        bar_list = filter(lambda i:i>=play_tick_list[0],bar_list)
+#        bar_list = filter(lambda i:i<=play_tick_list[3],bar_list)
+#        bar_list = map(lambda i:i-play_tick_list[0],bar_list)
+#        bar_list = list(bar_list)
+#
+#        bar_list0 = list(map(lambda i:i-play_tick_list[3]+play_tick_list[0],bar_list))
+#        bar_list1 = list(map(lambda i:i+play_tick_list[3]-play_tick_list[0],bar_list))
+#        bar_list = bar_list0 + bar_list + bar_list1
+#
+#        bar_set = set(bar_list)
+#        bar_list = sorted(bar_set)
+#        self.track_data['bar_list'] = bar_list
+#        self.track_data['bar_set'] = bar_set
+#        
+#        tempo_list = src_track_data['tempo_list']
+#        tempo_list = filter(lambda i:i['tick1']>play_tick_list[0],tempo_list)
+#        tempo_list = filter(lambda i:i['tick0']<=play_tick_list[3],tempo_list)
+#        tempo_list = list(tempo_list)
+#        for tempo in tempo_list:
+#            tempo['tick0'] -= play_tick_list[0]
+#            tempo['tick1'] -= play_tick_list[0]
+#            tempo['sec6tpb0'] -= play_sec6tpb_list[0]
+#            tempo['sec6tpb1'] -= play_sec6tpb_list[0]
+#        tempo_list[0]['tick0']    = 0
+#        tempo_list[0]['tick1']    = play_tick_list[3] - play_tick_list[0]
+#        tempo_list[0]['sec6tpb0'] = 0
+#        tempo_list[0]['sec6tpb1'] = play_sec6tpb_list[3] - play_sec6tpb_list[0]
+#        self.track_data['tempo_list'] = tempo_list
+#
+#        self.track_data['max_pitch'] = src_track_data['max_pitch']
+#        self.track_data['min_pitch'] = src_track_data['min_pitch']
+#        
+#        self.track_data['sec6tpb'] = play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#
+#        min_sec6tpb = play_sec6tpb_list[1]-play_sec6tpb_list[0]
+#        max_sec6tpb = play_sec6tpb_list[2]-play_sec6tpb_list[0]
+#        sec6tpb_30 = play_sec6tpb_list[3]-play_sec6tpb_list[0]
+#        play_noteev_list = filter(lambda i:i['type']=='on',play_noteev_list)
+#        play_noteev_list = list(play_noteev_list)
+#        for noteev in play_noteev_list:
+#            noteev['sec6tpb0'] = max(noteev['sec6tpb0'],min_sec6tpb)
+#            noteev['sec6tpb1'] = min(noteev['sec6tpb1'],max_sec6tpb)
+#            noteev['sec6tpb']  = noteev['sec6tpb0']
+#            noteev['sort_key'] = (noteev['sec6tpb'],1,noteev['pitch'])
+#        play_noteev_off_list = copy.deepcopy(play_noteev_list)
+#        for noteev in play_noteev_off_list:
+#            noteev['type'] = 'off'
+#            noteev['sec6tpb']  = noteev['sec6tpb1']
+#            noteev['sort_key'] = (noteev['sec6tpb'],0,noteev['pitch'])
+#        play_noteev_list = play_noteev_list + play_noteev_off_list
+#        play_noteev_list = sorted(play_noteev_list, key=lambda i:i['sort_key'])
 
-        self.track_data = {}
+        play_track_data = copy.deepcopy(self.runtime.midi_data['track_list'][0])
+        play_tick_list    = list(map(lambda i:i*play_track_data['ticks_per_beat'],self.runtime.play_beat_list))
+        play_sec6tpb_list = list(map(lambda i:midi_data.tick_to_sec6tpb(i,play_track_data['tempo_list']),play_tick_list))
+        play_track_data = midi_data.track_data_chop_tick(play_track_data, *play_tick_list)
+        play_track_data = midi_data.track_data_move_tick(play_track_data, -play_tick_list[0])
+        play_track_data = midi_data.track_data_move_sec6tpb(play_track_data, -play_sec6tpb_list[0])
 
-        noteev_list = src_track_data['noteev_list']
-        noteev_list = filter(lambda i:i['type']=='on', noteev_list)
-        noteev_list = filter(lambda i:i['tick1']>=play_tick_list[1],noteev_list)
-        noteev_list = filter(lambda i:i['tick0']<play_tick_list[2],noteev_list)
-        noteev_list = list(noteev_list)
-        noteev_list = copy.deepcopy(noteev_list)
-        for noteev in noteev_list:
-            noteev['tick']  -= play_tick_list[0]
-            if 'tick0' in noteev: noteev['tick0'] -= play_tick_list[0]
-            if 'tick1' in noteev: noteev['tick1'] -= play_tick_list[0]
-            noteev['sec6tpb']  -= play_sec6tpb_list[0]
-            if 'sec6tpb0' in noteev: noteev['sec6tpb0'] -= play_sec6tpb_list[0]
-            if 'sec6tpb1' in noteev: noteev['sec6tpb1'] -= play_sec6tpb_list[0]
-
-        play_noteev_list = copy.deepcopy(noteev_list)
-
-        noteev_list0 = copy.deepcopy(noteev_list)
-        for noteev in noteev_list0:
-            noteev['tick']  -= play_tick_list[3]-play_tick_list[0]
-            if 'tick0' in noteev: noteev['tick0'] -= play_tick_list[3]-play_tick_list[0]
-            if 'tick1' in noteev: noteev['tick1'] -= play_tick_list[3]-play_tick_list[0]
-            noteev['sec6tpb']  -= play_sec6tpb_list[3]-play_sec6tpb_list[0]
-            if 'sec6tpb0' in noteev: noteev['sec6tpb0'] -= play_sec6tpb_list[3]-play_sec6tpb_list[0]
-            if 'sec6tpb1' in noteev: noteev['sec6tpb1'] -= play_sec6tpb_list[3]-play_sec6tpb_list[0]
-
-        noteev_list1 = copy.deepcopy(noteev_list)
-        for noteev in noteev_list1:
-            noteev['tick']  += play_tick_list[3]-play_tick_list[0]
-            if 'tick0' in noteev: noteev['tick0'] += play_tick_list[3]-play_tick_list[0]
-            if 'tick1' in noteev: noteev['tick1'] += play_tick_list[3]-play_tick_list[0]
-            noteev['sec6tpb']  += play_sec6tpb_list[3]-play_sec6tpb_list[0]
-            if 'sec6tpb0' in noteev: noteev['sec6tpb0'] += play_sec6tpb_list[3]-play_sec6tpb_list[0]
-            if 'sec6tpb1' in noteev: noteev['sec6tpb1'] += play_sec6tpb_list[3]-play_sec6tpb_list[0]
-
-        noteev_list = noteev_list0+noteev_list+noteev_list1
-
-        self.track_data['noteev_list'] = noteev_list
-        
-        bar_list = src_track_data['bar_list']
-        bar_list = filter(lambda i:i>=play_tick_list[0],bar_list)
-        bar_list = filter(lambda i:i<=play_tick_list[3],bar_list)
-        bar_list = map(lambda i:i-play_tick_list[0],bar_list)
-        bar_list = list(bar_list)
-
-        bar_list0 = list(map(lambda i:i-play_tick_list[3]+play_tick_list[0],bar_list))
-        bar_list1 = list(map(lambda i:i+play_tick_list[3]-play_tick_list[0],bar_list))
-        bar_list = bar_list0 + bar_list + bar_list1
-
-        bar_set = set(bar_list)
-        bar_list = sorted(bar_set)
-        self.track_data['bar_list'] = bar_list
-        self.track_data['bar_set'] = bar_set
-        
-        tempo_list = src_track_data['tempo_list']
-        tempo_list = filter(lambda i:i['tick1']>play_tick_list[0],tempo_list)
-        tempo_list = filter(lambda i:i['tick0']<=play_tick_list[3],tempo_list)
-        tempo_list = list(tempo_list)
-        for tempo in tempo_list:
-            tempo['tick0'] -= play_tick_list[0]
-            tempo['tick1'] -= play_tick_list[0]
-            tempo['sec6tpb0'] -= play_sec6tpb_list[0]
-            tempo['sec6tpb1'] -= play_sec6tpb_list[0]
-        tempo_list[0]['tick0']    = 0
-        tempo_list[0]['tick1']    = play_tick_list[3] - play_tick_list[0]
-        tempo_list[0]['sec6tpb0'] = 0
-        tempo_list[0]['sec6tpb1'] = play_sec6tpb_list[3] - play_sec6tpb_list[0]
-        self.track_data['tempo_list'] = tempo_list
-
-        self.track_data['max_pitch'] = src_track_data['max_pitch']
-        self.track_data['min_pitch'] = src_track_data['min_pitch']
-        
-        self.track_data['sec6tpb'] = play_sec6tpb_list[3]-play_sec6tpb_list[0]
-
-        min_sec6tpb = play_sec6tpb_list[1]-play_sec6tpb_list[0]
-        max_sec6tpb = play_sec6tpb_list[2]-play_sec6tpb_list[0]
+        tick_30    = play_tick_list[3]-play_tick_list[0]
         sec6tpb_30 = play_sec6tpb_list[3]-play_sec6tpb_list[0]
-        play_noteev_list = filter(lambda i:i['type']=='on',play_noteev_list)
-        play_noteev_list = list(play_noteev_list)
-        for noteev in play_noteev_list:
-            noteev['sec6tpb0'] = max(noteev['sec6tpb0'],min_sec6tpb)
-            noteev['sec6tpb1'] = min(noteev['sec6tpb1'],max_sec6tpb)
-            noteev['sec6tpb']  = noteev['sec6tpb0']
-            noteev['sort_key'] = (noteev['sec6tpb'],1,noteev['pitch'])
-        play_noteev_off_list = copy.deepcopy(play_noteev_list)
-        for noteev in play_noteev_off_list:
-            noteev['type'] = 'off'
-            noteev['sec6tpb']  = noteev['sec6tpb1']
-            noteev['sort_key'] = (noteev['sec6tpb'],0,noteev['pitch'])
-        play_noteev_list = play_noteev_list + play_noteev_off_list
-        play_noteev_list = sorted(play_noteev_list, key=lambda i:i['sort_key'])
+        #print(f'YDXUFZLYQK tick_30={tick_30}, sec6tpb_30={sec6tpb_30}, play_tick_list={play_tick_list}, play_sec6tpb_list={play_sec6tpb_list}')
+        display_track_data = copy.deepcopy(play_track_data)
+        display_track_data0 = copy.deepcopy(display_track_data)
+        display_track_data0 = midi_data.track_data_move_tick(display_track_data0, -tick_30)
+        display_track_data0 = midi_data.track_data_move_sec6tpb(display_track_data0, -sec6tpb_30)
+        display_track_data1 = copy.deepcopy(display_track_data)
+        display_track_data1 = midi_data.track_data_move_tick(display_track_data1, tick_30)
+        display_track_data1 = midi_data.track_data_move_sec6tpb(display_track_data1, sec6tpb_30)
+        for dm in display_track_data0['noteev_list']: dm['src']='0'
+        for dm in display_track_data['noteev_list']:  dm['src']='1'
+        for dm in display_track_data1['noteev_list']: dm['src']='2'
+        display_track_data = midi_data.merge_track_data([display_track_data,display_track_data0,display_track_data1])
 
-        self.runtime.midi_player.play(play_noteev_list,sec6tpb_30,self.runtime.midi_data['ticks_per_beat'],self.start_sec-0.15)
+        self.track_data = display_track_data
+        self.loop_sec6tpb = sec6tpb_30
+
+        self.runtime.midi_player.play(play_track_data['noteev_list'],sec6tpb_30,play_track_data['ticks_per_beat'],self.start_sec-0.15)
 
         super().on_active()
 
