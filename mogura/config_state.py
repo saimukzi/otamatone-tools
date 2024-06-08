fb7cf0eaaf795132726f59aa1b06a17cd3d01619
saimukzi <saimukzi@hiauntie.com>
2024-06-08 14:35:08 +0800

    add view direction config

--- mogura/config_state.py
+++ mogura/config_state.py
@@ -3,6 +3,7 @@ import gui
 import null_state
 import pygame
 import user_data
+import mgr_enum
 
 class ConfigState(null_state.NullState):
 
@@ -12,6 +13,17 @@ class ConfigState(null_state.NullState):
 
     def screen_tick(self, screen, text_draw, sec, **kwargs):
         super().screen_tick(screen=screen, sec=sec, text_draw=text_draw, **kwargs)
+        txt = 'TH+PV' if ((self.runtime.config['ui_time_direction'] & mgr_enum.HORI_MASK) and (self.runtime.config['ui_pitch_direction'] & mgr_enum.VERT_MASK)) else \
+              'TV+PH' if ((self.runtime.config['ui_time_direction'] & mgr_enum.VERT_MASK) and (self.runtime.config['ui_pitch_direction'] & mgr_enum.HORI_MASK)) else \
+              'UNKNOWN'
+        txt = f'Pitch/Time: {txt}'
+        self.gui.set_label_text('ui_pitch_time_direction.text', txt)
+        txt = mgr_enum.DIRECTION_TO_NAME[self.runtime.config['ui_time_direction']]
+        txt = f'Time: {txt}'
+        self.gui.set_label_text('ui_time_direction.text', txt)
+        txt = mgr_enum.DIRECTION_TO_NAME[self.runtime.config['ui_pitch_direction']]
+        txt = f'Pitch: {txt}'
+        self.gui.set_label_text('ui_pitch_direction.text', txt)
         self.gui.draw_layer('options', screen, text_draw)
         self.gui.draw_layer('back', screen, text_draw)
 
@@ -29,6 +41,15 @@ class ConfigState(null_state.NullState):
             self.runtime.state_pool.set_active('CONFIG_AUDIO_INPUT')
         if self.gui.is_btn_active('audio_output.click'):
             self.runtime.state_pool.set_active('CONFIG_AUDIO_OUTPUT')
+
+        if self.gui.is_btn_active('ui_pitch_time_direction.click'):
+            tmp = self.runtime.config['ui_time_direction']
+            self.runtime.config['ui_time_direction'] = self.runtime.config['ui_pitch_direction']
+            self.runtime.config['ui_pitch_direction'] = tmp
+        if self.gui.is_btn_active('ui_time_direction.click'):
+            self.runtime.config['ui_time_direction'] = mgr_enum.DIRECTION_TO_OPPOSITE[self.runtime.config['ui_time_direction']]
+        if self.gui.is_btn_active('ui_pitch_direction.click'):
+            self.runtime.config['ui_pitch_direction'] = mgr_enum.DIRECTION_TO_OPPOSITE[self.runtime.config['ui_pitch_direction']]
 
     def on_active(self):
         self.update_ui_matrice()
@@ -51,6 +72,15 @@ class ConfigState(null_state.NullState):
         y += 50
         self.gui.add_label('audio_output.text','Audio Output',const.FONT_SIZE,(127,127,127), (x,y), 7,'options')
         self.gui.add_click('audio_output.click', (x,y), (240,40), 7, 'options')
+        y += 75
+        self.gui.add_label('ui_pitch_time_direction.text','',const.FONT_SIZE,(127,127,127), (x,y), 7,'options')
+        self.gui.add_click('ui_pitch_time_direction.click', (x,y), (240,40), 7, 'options')
+        y += 50
+        self.gui.add_label('ui_time_direction.text','',const.FONT_SIZE,(127,127,127), (x,y), 7,'options')
+        self.gui.add_click('ui_time_direction.click', (x,y), (240,40), 7, 'options')
+        y += 50
+        self.gui.add_label('ui_pitch_direction.text','',const.FONT_SIZE,(127,127,127), (x,y), 7,'options')
+        self.gui.add_click('ui_pitch_direction.click', (x,y), (240,40), 7, 'options')
 
         self.gui.add_label('back.text','Back',const.FONT_SIZE,(127,127,127), (width-10,height-10), 3,'back')
         self.gui.add_click('back.click', (width-10,height-10), (240,40), 3, 'back')
